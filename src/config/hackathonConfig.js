cacee7ee42f78ee0e63c4c3eab0627ed7d18ad25
AnushkaChoudhari <anushkachoudhari5@gmail.com>
2026-08-16 16:53:09 +0530

    final

--- src/config/hackathonConfig.js
+++ src/config/hackathonConfig.js
@@ -7,7 +7,7 @@ export const hackathonConfig = {
   USE_DUMMY_DATA: false,
 
   // Google Integration Endpoints
-  GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzEV6qQtoFa1gJxha7kAP2XazR2cUzUaa8H_Pkw-NluBZXN5fl5MMaQNm_91lILZy2HBw/exec",
+  GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxfaU01lad4YbmIRmwwMIwHC0APPdbLO21uq25OwwH8jItcKnv4DOi5I6dEtf_FpQV5Pg/exec",
   JURY_GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxviqHzHJIQGXEOi3nqB63UeQqLOhniWZ-dEWXg8SlaL8x-30SHE0Wo4IzrOYMMyH7c/exec",
   GOOGLE_DRIVE_FOLDER_ID: "YOUR_GOOGLE_DRIVE_FOLDER_ID",
 
